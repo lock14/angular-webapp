8b934916ccc424e404fba5579b02f3dd78034aa0
Brian Bechtel <brianbechtel@outlook.com>
2019-11-25 23:15:07 -0800

    made it so npm wrapper scripts install local copy if its not present

--- npmw.cmd
+++ npmw.cmd
@@ -1,1 +1,9 @@
-.\angular\node\node.exe ".\angular\node\node_modules\npm\bin\npm-cli.js" --scripts-prepend-node-path true --prefix angular %*
+@echo off
+SETLOCAL
+
+SET LOCAL_NODE=.\angular\node\node.exe
+SET LOCAL_NPM=.\angular\node\node_modules\npm\bin\npm-cli.js
+
+IF NOT EXIST %LOCAL_NODE% .\mvnw.cmd generate-resources
+
+%LOCAL_NODE% "%LOCAL_NPM%" --scripts-prepend-node-path true --prefix angular %*
